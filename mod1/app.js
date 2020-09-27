86798b02358530b88842cc97b430affdca2c5b12
unknown <riteshofficial1102@gmail.com>
2020-09-27 15:30:03 +0545

    Final shot

--- mod1/app.js
+++ mod1/app.js
@@ -1,9 +1,35 @@
 (function () {
 	'use strict';
 	angular.module('LunchCheck',[])
-	.controller('LunchCheckController', function($scope,$injector){
-		
-	});
-		
+	.controller('LunchCheckController', LunchCheckController);
+	LunchCheckController.$inject = ['$scope'];
+
+	function LunchCheckController($scope){
+		$scope.message="";
+		$scope.namesOfDishes="";
+		$scope.checkDishes = function(){
+			$scope.message = messageForDishes($scope.namesOfDishes);
+		};
+	}
+	function messageForDishes(namesOfDishes){
+		if (namesOfDishes.split(',')=="") {
+			return "Enter the data first";
+		} else if (numberOfDishes(namesOfDishes) <= 3) {
+			return "Enjoy";
+		} else {
+			return "Too much!";
+		}
+	}
+	function numberOfDishes(namesOfDishes){
+		var items = namesOfDishes.split(',');
+		var numberOfItems = 0;
+		for (var i = 0; i < items.length; i++) {
+			if (items[i].trim()!=""){
+				numberOfItems++;
+			}
+		}
+		return numberOfItems;
+	}
+					
 	
 })();
